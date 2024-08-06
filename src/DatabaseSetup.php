dbf108272ccc8e28018141f2d64c9c357f8be35a
jaesonarsoftware <80274762+jaesonarsoftware@users.noreply.github.com>
2024-08-06 14:25:49 -0400

    Update DatabaseSetup.php

--- src/DatabaseSetup.php
+++ src/DatabaseSetup.php
@@ -115,20 +115,31 @@ class DatabaseSetup
      */
     public function addRemoteAccessUser()
     {
-        $input = $this->climate->lightBlue()->input("What is the IP address of the remote server that will be accessing the database?");
+        $input = $this->climate->lightBlue()->input("Enter 'd' for Sonar defaults or IP address of the remote server that will be accessing the database.");
+        $ipAddresses = [];
         $ipAddress = null;
         while ($ipAddress == null || filter_var($ipAddress, FILTER_VALIDATE_IP) === false)
         {
             $ipAddress = $input->prompt();
             if ($ipAddress == null)
             {
-                $this->climate->shout("You must input an IP address.");
+                $this->climate->shout("You must input an IP address or 'd' for Sonar defaults.");
+            }
+            elseif ($ipAddress === 'd')
+            {
+                $this->climate->lightMagenta("Using egress IPs of Sonar..");
+                $ipAddresses = ['20.221.112.37', '20.221.114.13', '52.158.209.86'];
             }
             elseif (filter_var($ipAddress, FILTER_VALIDATE_IP) === false)
             {
                 $this->climate->shout("That IP address is not valid.");
             }
+            else
+            {
+                $ipAddresses[] = $ipAddress;
+            }
         }
+        
 
         $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $username = '';
@@ -142,15 +153,17 @@ class DatabaseSetup
         }
 
         $sth = $this->dbh->prepare("GRANT ALL ON radius.* TO ?@? IDENTIFIED BY ?");
-        if ($sth->execute([$username, $ipAddress, $password]))
-        {
-            $sth = $this->dbh->prepare("FLUSH PRIVILEGES");
-            $sth->execute();
-            $this->climate->lightMagenta("Added a user with the username $username and the password $password. Copy this username and password, you'll need it!");
-        }
-        else
-        {
-            $this->climate->shout("Failed to create the user!");
+        for ($i=0; $i < count($ipAddresses); i++) {
+            if ($sth->execute([$username, $ipAddresses[$i], $password]))
+            {
+                $sth = $this->dbh->prepare("FLUSH PRIVILEGES");
+                $sth->execute();
+                $this->climate->lightMagenta("Added a user with the username $username and the password $password. Copy this username and password, you'll need it!");
+            }
+            else
+            {
+                $this->climate->shout("Failed to create the user!");
+            }
         }
     }
 
