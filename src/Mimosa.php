cac33f2aa37c4766a1e8e3391851b545090359c6
ethan-thompson <ethan.thompson@networkradius.com>
2025-05-26 18:51:35 +0000

    Update file and path names to match what is used in FreeRadius v3
    Signed-off-by: ethan-thompson <ethan.thompson@networkradius.com>

--- src/Mimosa.php
+++ src/Mimosa.php
@@ -17,18 +17,17 @@ class Mimosa
     {
         $this->climate->lightBlue()->inline("Enabling EAP and adding Mimosa dictionary... ");
         try {
-            CommandExecutor::executeCommand("/bin/rm -f " . __DIR__ . "etc/freeradius/eap.conf");
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/eap.conf /etc/freeradius/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/eap /etc/freeradius/3.0/mods-available/");
 
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/*.pem /etc/freeradius/certs/");
-            CommandExecutor::executeCommand("(cd /etc/freeradius/certs; /usr/bin/c_rehash .)");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/*.pem /etc/freeradius/3.0/certs/");
+            CommandExecutor::executeCommand("(cd /etc/freeradius/3.0/certs; /usr/bin/c_rehash .)");
 
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/dictionary.mimosa /etc/freeradius/");
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/dictionary /etc/freeradius/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/dictionary.mimosa /etc/freeradius/3.0/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/thirdparty/mimosa/dictionary /etc/freeradius/3.0/");
 
             //Copy this over again due to the EAP changes that may be missing
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/default /etc/freeradius/sites-available/");
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/inner-tunnel /etc/freeradius/sites-available/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/default /etc/freeradius/3.0/sites-available/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/inner-tunnel /etc/freeradius/3.0/sites-available/");
 
             CommandExecutor::executeCommand("/usr/sbin/service freeradius restart");
         }
