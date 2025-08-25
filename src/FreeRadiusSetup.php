dcd318850fb3667e93408b053d9f72e243c6d704
ethan-thompson <ethan.thompson@networkradius.com>
2025-08-25 10:58:20 -0400

    fix: Resolved issue where sql module was not being enabled and run with the correct configs

--- src/FreeRadiusSetup.php
+++ src/FreeRadiusSetup.php
@@ -23,9 +23,10 @@ class FreeRadiusSetup
         $this->climate->lightBlue()->inline("Configuring FreeRADIUS to use the SQL database... ");
         try {
             CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/radiusd.conf /etc/freeradius/3.0/");
-            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/sql /etc/freeradius/3.0/mods-enabled/");
+            CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/sql /etc/freeradius/3.0/mods-available/");
             CommandExecutor::executeCommand("/bin/cp " . __DIR__ . "/../conf/default /etc/freeradius/3.0/sites-available/");
             CommandExecutor::executeCommand("/bin/sed -i 's/password = \"radpass\"/password = \"$mysqlPassword\"/g' /etc/freeradius/3.0/mods-available/sql");
+            CommandExecutor::executeCommand("/bin/ln -sf /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/sql");
             CommandExecutor::executeCommand("/usr/sbin/service freeradius restart");
         }
         catch (RuntimeException $e)
